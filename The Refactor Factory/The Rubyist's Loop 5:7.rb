fcac3b36bd795af1f47af9f572b67dbed51232aa
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-06-30 19:44:50 +0100

    codecademy - The Refactor Factory

---
+++ The Refactor Factory/The Rubyist's Loop 5:7.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby -w
+
+$VERBOSE = nil    # We'll explain this at the end of the lesson.
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+
+ 
+    return "n must be an integer." unless n.is_a? Integer
+
+
+  
+    return "n must be greater than 0." if n <= 0
+
+  
+  prime_array ||= []
+  
+  prime = Prime.new
+  n.times{prime_array.push(prime.next)}
+  prime_array
+end
+
+first_n_primes(10)
